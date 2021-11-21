613d59ad48f96f90c0cc72633c36f9b62e4eb341
Solmarnov <solmarnov@gmail.com>
2021-11-22 09:27:30 +1100

    app prep

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,2 +1,17 @@
 import React, { useState } from 'react'
 import API from '../../utils/AtomicHubAPI'
+
+
+
+
+const Search = () => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+
+}
+
+export default Search;

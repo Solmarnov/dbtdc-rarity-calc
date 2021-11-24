0098aed5b7efe075ec0e5b8f9a667f7c8e136fc5
Solmarnov <solmarnov@gmail.com>
2021-11-24 23:18:39 +1100

    header component updated

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,11 +3,13 @@ import './style.css'
 const Header = () => {
 
   return (
-    <header class="primary-header">
+    <header className="primary-header">
       <div>
-        <img src="" class="logo" />
+        <a className="logo-wrap" href="#">
+          <img src="" className="logo" role="img" aria-label="Site home"/>
+          <span className="logo-text">The Dropbears Tree Diving Club</span>
+        </a>
       </div>
-
       <div className="menu-wrap">
         <input type="checkbox" className="toggler" />
         <div className="hamburger">
@@ -18,7 +20,7 @@ const Header = () => {
             <div>
               <ul>
                 <li><a href="https://sites.google.com/dayum.com.au/dropbears/home?authuser=0">Home</a></li>
-                <li><a href="https://sites.google.com/dayum.com.au/dropbears/rarity?authuser=0">Rarity</a></li>
+                <li><a class="active" href="https://sites.google.com/dayum.com.au/dropbears/rarity?authuser=0">Rarity</a></li>
                 <li><a href="https://sites.google.com/dayum.com.au/dropbears/events?authuser=0">Events</a></li>
                 <li><a href="https://sites.google.com/dayum.com.au/dropbears/whats-a-dropbear?authuser=0">What's a Dropbear?</a></li>
               </ul>
